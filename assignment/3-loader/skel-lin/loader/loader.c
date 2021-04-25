aacee9a128728b137decb5c33bc4f4cad8ee4ba9
nummada <madalina.nuta1@gmail.com>
2021-04-25 13:22:05 +0300

    bss

--- assignment/3-loader/skel-lin/loader/loader.c
+++ assignment/3-loader/skel-lin/loader/loader.c
@@ -31,9 +31,9 @@ typedef struct page_info {
 
 int getSegmentPositionOfAddr(char *addr)
 {
-	int index = SEGMENT_INDEX_ERROR;
+	int index = SEGMENT_INDEX_ERROR, i;
 
-	for(int i = 0 ; i < exec->segments_no ; i++) {
+	for(i = 0 ; i < exec->segments_no ; i++) {
 		if (addr >= (char*)(exec->segments[i].vaddr) && addr <= (char*)(exec->segments[i].vaddr + exec->segments[i].mem_size)) {
 			index = i;
 			break;
@@ -54,7 +54,7 @@ void initSegmentData(int segment_index)
 	printf("no of pages: [%d]\n", number_of_pages);
 }
 
-char* call_mmap(int segment_index, int page) {
+char* call_mmap_on_file(int segment_index, int page) {
 	return mmap((void*) exec->segments[segment_index].vaddr +
 			pageSize * page,
 			pageSize,
@@ -64,27 +64,74 @@ char* call_mmap(int segment_index, int page) {
 			exec->segments[segment_index].offset + pageSize * page);
 }
 
-void legal_page_fault(int segment_index, int page) {
+char* call_anonymous_mmap(int segment_index, int page) {
+	return mmap((void*) exec->segments[segment_index].vaddr +
+			pageSize * page,
+			pageSize,
+			exec->segments[segment_index].perm,
+			MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS,
+			-1,
+			0);
+}
+
+char* call_mmap_zero(int segment_index, int page, int size) {
+	return mmap((void*) exec->segments[segment_index].vaddr +
+			pageSize * page,
+			size,
+			exec->segments[segment_index].perm,
+			MAP_FIXED | MAP_PRIVATE,
+			fd,
+			exec->segments[segment_index].offset + pageSize * page);
+}
+
+void legal_page_fault(int segment_index, int page, char *addr) {
 	char *mmap_addr;
-	unsigned int mem_size = exec->segments[segment_index].mem_size;
-	unsigned int file_size = exec->segments[segment_index].file_size;
-	int size_to_write = pageSize;
+	size_t mem_size = exec->segments[segment_index].mem_size;
+	size_t file_size = exec->segments[segment_index].file_size;
+	size_t zero_mem_size = 0;
+	int start_page_index = exec->segments[segment_index].vaddr +
+			pageSize * page;
+	int end_page_index = exec->segments[segment_index].vaddr +
+			pageSize * page + pageSize;
+	int start_page_offset = pageSize * page;
+	int end_page_offset = pageSize * (page + 1) - 1;
 
+	printf("mapez [%d] -> [%d]\n", start_page_index, end_page_index);
 
-	printf("mapez [%d] -> [%d]\n", exec->segments[segment_index].vaddr +
-			pageSize * page, exec->segments[segment_index].vaddr +
-			pageSize * page + pageSize);
-
-	mmap_addr = call_mmap(segment_index, page);
-	if (mmap_addr == MAP_FAILED) {
-		printf("map failed\n");
+	if (mem_size == file_size) {
+		mmap_addr = call_mmap_on_file(segment_index, page);
+		if (mmap_addr == MAP_FAILED) {
+			printf("map failed\n");
+		}
+		printf("egale!!!!\n");
 	}
-	// printf("res: [%d]\n", res);
-	((page_info_t*)exec->segments[segment_index].data)[page].mapped = 1;
+
 
 	if (mem_size > file_size) {
-		size_to_write -= (file_size - page * pageSize);
+		printf("BSS [%d] [%d] [%d] \n", start_page_offset,  end_page_offset, file_size);
+		if (exec->segments[segment_index].file_size > start_page_offset
+			&& exec->segments[segment_index].file_size <= end_page_offset) {
+			printf("inauntru \n");
+			zero_mem_size = end_page_offset - file_size;
+			mmap_addr = call_mmap_zero(segment_index, page, file_size - start_page_offset);
+			// printf("size: [%d]\n", zero_mem_size);
+			mprotect(mmap_addr, pageSize, PROT_WRITE);
+			int res = memset(mmap_addr + file_size - start_page_offset, 0, zero_mem_size);
+			// printf("res [%d]\n", res);
+			mprotect(mmap_addr, pageSize, exec->segments[segment_index].perm);
+		} else if (file_size <= start_page_offset) {
+			mmap_addr = call_anonymous_mmap(segment_index, page);
+			// printf("mmap returns [%p]", mmap_addr);
+			if (mmap_addr == MAP_FAILED) {
+				printf("map failed\n");
+			}
+			printf("inafara \n");
+			// mprotect(mmap_addr, pageSize, PROT_WRITE);
+			// memset(mmap_addr, 0, pageSize);
+			// mprotect(mmap_addr, pageSize, exec->segments[segment_index].perm);
+		}
 	}
+	((page_info_t*)exec->segments[segment_index].data)[page].mapped = 1;
 }
 
 static void segv_handler(int signum, siginfo_t *info, void *context)
@@ -97,7 +144,7 @@ static void segv_handler(int signum, siginfo_t *info, void *context)
 
 	segment_index = getSegmentPositionOfAddr(addr);
 	if (segment_index == SEGMENT_INDEX_ERROR) {
-		printf("segfault la [%d]\n", addr);
+		// printf("segfault la [%d]\n", addr);
 		old_action.sa_sigaction(signum, info, context);
 	}
 
@@ -105,16 +152,14 @@ static void segv_handler(int signum, siginfo_t *info, void *context)
 	if (exec->segments[segment_index].data == NULL) {
 		initSegmentData(segment_index);
 	}
-	printf("segment [%d]", segment_index);
-	printf("page [%d]", page);
+	// printf("segment [%d]", segment_index);
+	// printf("page [%d]", page);
 	data = exec->segments[segment_index].data;
 	if (data[page].mapped == 1) {
 		old_action.sa_sigaction(signum, info, context);
 	} else {
-		legal_page_fault(segment_index, page);
+		legal_page_fault(segment_index, page, addr);
 	}
-	
-	
 }
 
 
